79af63edc8297d0b742e31b9170fe589a3b232a3
Aisha Emin <aishaemin.sdet@gmail.com>
2026-03-11 12:42:22 -0400

    Fix API session authentication

--- src/test/java/base/BaseApiTest.java
+++ src/test/java/base/BaseApiTest.java
@@ -3,6 +3,7 @@ package base;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,21 +11,45 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import utils.ConfigManager;
 import utils.TestListener;
 
+import static io.restassured.RestAssured.given;
 
 @ExtendWith(TestListener.class)
 public class BaseApiTest {
 
     protected static RequestSpecification requestSpec;
     protected static ResponseSpecification responseSpec;
+    protected static String sessionId;
 
     @BeforeAll
     static void setup() {
 
         RestAssured.baseURI = ConfigManager.getApiBaseUrl();
 
+        // LOGIN → get session
+        Response response =
+                given()
+                        .auth()
+                        .preemptive()
+                        .basic(
+                                ConfigManager.getUsername(),
+                                ConfigManager.getPassword()
+                        )
+                        .when()
+                        .get("/session")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .response();
+
+        sessionId = response.cookie("JSESSIONID");
+
+        System.out.println("Session ID: " + sessionId);
+
+        // Request specification with authentication cookie
         requestSpec = new RequestSpecBuilder()
                 .setBaseUri(RestAssured.baseURI)
                 .setContentType("application/json")
+                .addCookie("JSESSIONID", sessionId)
                 .build();
 
         responseSpec = new ResponseSpecBuilder()
