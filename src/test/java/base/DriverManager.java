2f9bf25d7da22b768f22499fdc47862ae3158986
Aisha Emin <aishaemin.sdet@gmail.com>
2026-03-11 12:21:14 -0400

    Fix Selenium CI by enabling headless Chrome

--- src/test/java/base/DriverManager.java
+++ src/test/java/base/DriverManager.java
@@ -2,13 +2,23 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
 
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static void initDriver() {
-        driver.set(new ChromeDriver());
+
+        ChromeOptions options = new ChromeOptions();
+
+        // Required for GitHub Actions (Linux)
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+
+        driver.set(new ChromeDriver(options));
     }
 
     public static WebDriver getDriver() {
@@ -16,7 +26,10 @@ public class DriverManager {
     }
 
     public static void quitDriver() {
-        driver.get().quit();
-        driver.remove();
+
+        if (driver.get() != null) {
+            driver.get().quit();
+            driver.remove();
+        }
     }
 }
